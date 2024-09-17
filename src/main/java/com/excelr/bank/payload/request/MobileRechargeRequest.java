3890454aa8088322eded689f459fc0571f2e80ad
Shubh <shubham.mittal843@gmail.com>
2024-09-17 22:25:13 +0530

    New Api Recharge and Some Changes in Other API

---
+++ src/main/java/com/excelr/bank/payload/request/MobileRechargeRequest.java
@@ -0,0 +1,23 @@
+package com.excelr.bank.payload.request;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MobileRechargeRequest {
+
+    @Pattern(regexp = "^(\\+\\d{1,2}\\s?)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$")
+    @Size(min = 10, max = 13)
+    private String mobileNumber;
+
+    private BigDecimal rechargeAmount;
+
+}
